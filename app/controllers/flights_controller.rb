f98d9daed4dd5d4daa1ec1f751f3487029f20dca
RunTheLi <lanmarock@gmail.com>
2025-05-25 13:00:45 +0700

    add: available flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,18 +2,18 @@ class FlightsController < ApplicationController
   def index
     @airport = Airport.all
     @available_dates = Flight.pluck(:departure_time).map { |dt| dt.to_date }.uniq.sort
+    @flight = []
 
     if params[:departure_airport_id].present? &&
        params[:arrival_airport_id].present? &&
-       params[:search] && params[:search][:date].present?
+       params[:search].present? && params[:search][:date].present?
 
       date = Date.parse(params[:search][:date])
-      @flight = Flight.where(
+
+      @flights = Flight.where(
         departure_airport_id: params[:departure_airport_id],
         arrival_airport_id: params[:arrival_airport_id]
       ).where("DATE(departure_time) = ?", date)
-    else
-      @flights = []
     end
   end
 end
